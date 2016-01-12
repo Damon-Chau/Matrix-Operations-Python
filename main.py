d62a5d1cc81a60755fee0750bc33dc26d6e1a0ee
Damon Chau <damon.hmchau@gmail.com>
2016-01-12 11:34:37 -0500

    Input functions
    Created input functions

---
+++ main.py
@@ -0,0 +1,22 @@
+def matrix_size():
+    # user inputs the size of a matrix m x n (rows x columns)
+
+    rows = input("Enter the number of rows in the matrix:")
+    columns = input("Enter the number of columns" )
+
+
+def matrix_input(rows, columns, matrix):
+    # fill matrix with values
+
+    for i in range (rows):
+        for j in range(columns):
+            matrix[i][j] = input("Enter a number for the matrix")
+
+
+def matrix_addition(matrix1, matrix2):
+    # add 2 matrices
+    a = 0 # temporary placeholder for content
+
+
+def main():
+    rows, columns = 0
